0152346ec7e9606879b32e766ae25da02b7be71b
rajsingh19 <rajsingh76554@gmail.com>
2024-05-19 01:49:43 +0530

    link updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -96,7 +96,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/rajsingh19"
                 target="_blank"
                 className="fork-btn-inner"
               >
