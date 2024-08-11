c71fc7853588123160301fd3088f5357fdfa4a46
Raj Singh <rajsingh76554@gmail.com>
2024-08-11 22:42:51 +0530

    done

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,10 @@ import VRROOM from "../../Assets/Projects/VR ROOM.png";
 import Lion from "../../Assets/Projects/Lion.png";
 import GPS from "../../Assets/Projects/GPS.jpg";
 import Butterfly from "../../Assets/Projects/Butterfly.png";
+import society from "../../Assets/Projects/society.jpg";
+import transitionmap  from "../../Assets/Projects/transition map.jpg";
+import heavenestate  from "../../Assets/Projects/heavenestate.jpg";
+
 
 function Projects() {
   return (
@@ -21,6 +25,18 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={society}
+              isBlog={false}
+              title="Real Estate -AR"
+              description="About
+              I am creating an AR app where, when a user scans a target image, virtual buildings appear in the real world. The user can rotate these buildings to view them in 360 degrees within the real world."
+              
+             
+              
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Lion}
@@ -28,6 +44,17 @@ function Projects() {
               title="Lion Mask Filter"
               description="I designed a LION mask filter on Spark AR for Instagram. When applied, users' faces are seamlessly adorned with a majestic lion mask, adding a touch of wildness to their selfies."
               ghLink="https://github.com/rajsingh19/LION-MASK-FILTER"
+              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={heavenestate}
+              isBlog={false}
+              title="VirtuBuild -APK"
+              description="About
+             VirtuBuild is a ar/vr marketing platform where users can have a look at properties before it is built in AR and VR encouraging remote marketing,informed decision making and save staging costs for builders"
               
             />
           </Col>
@@ -45,46 +72,17 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={VRROOM}
+              imgPath={transitionmap}
               isBlog={false}
-              title="Interview Room"
-              description="I developed a comprehensive virtual platform designed to facilitate the learning of soft skills. This platform comprises various interactive panels, including a virtual meeting room, an interview room, and an auditorium. Each of these panels serves as real-life scenarios where users can practice and enhance their soft skills.
-
-               AI models play the role of interviewers. These AI models are programmed to conduct mock interviews. They ask questions, evaluate responses, and offer feedback to help users improve their interviewing skills. The virtual meeting room allows users to participate in or conduct virtual meetings, fostering skills like communication, teamwork, and leadership. The auditorium is designed for presentations and public speaking practice, enabling users to build confidence and polish their presentation abilities. 
+              title="Real Estate -AR"
+              description="About
+              I am creating an AR app where users can change the buildings and view the modified ones. In this app, the buildings also auto-rotate."
               
-              This platform offers a comprehensive, immersive learning experience, leveraging AI technology to provide practical, hands-on training in essential soft skills."
-              ghLink="https://github.com/rajsingh19/Interview-Room"
-                       
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Devil}
-              isBlog={false}
-              title="Devil-Filter"
-              description="Create a devil face filter in Spark AR Studio by adding horns, red skin, and glowing eyes, then publish it to Instagram for users to apply in their stories and posts."
-              ghLink="https://github.com/rajsingh19/Devil-Filter"
+             
               
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={GPS}
-              isBlog={false}
-              title="Location Tracker"
-              description="I developed a GPS tracker project utilizing Unity3D with ARKit and ARCore to place AR objects at specific GPS coordinates. This project leverages Mapbox for precise geolocation and integrates GameSparks for backend data management. Here's a step-by-step guide:
-
-              1. **Mapbox Integration**: Set up Mapbox SDK in Unity to accurately track GPS locations.
-              2. **ARKit and ARCore**: Use these frameworks to overlay AR objects at designated GPS coordinates, providing an immersive augmented reality experience.
-              3. **GameSparks Database**: Implement GameSparks to store and retrieve messages tagged at specific locations, ensuring data persistence and easy access.
-              
-              This setup not only creates a robust GPS-based AR experience but also encourages social interaction through geotagged messages and AR content."
-              ghLink="https://github.com/rajsingh19/Location-Tracker/tree/main"
-              
-            />
-          </Col>
+  
 
           <Col md={4} className="project-card">
             <ProjectCard
@@ -98,6 +96,10 @@ function Projects() {
               
             />
           </Col>
+
+          
+         
+         
         </Row>
       </Container>
     </Container>
